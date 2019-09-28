67fc6c78164688da672893f579a2607d02152c79
Ufuk Çelen <ufuk_celen@me.com>
2019-09-28 18:58:28 +0300

    Initial Commit

---
+++ Suphe-Pencereler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Suphe-Pencereler
+//
+//  Created by Ufuk Celen on 24.09.2019.
+//  Copyright © 2019 Ufuk Celen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
